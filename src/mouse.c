a626717fc692559d86a714fe40aa2bb39f011aba
twinaphex <libretro@gmail.com>
2015-09-09 21:48:04 +0200

    No need for mouse

--- src/mouse.c
+++ src/mouse.c
@@ -22,11 +22,7 @@
 #include "video.h"
 #include "vga256d.h"
 
-#if defined(TARGET_GP2X) || defined(TARGET_DINGUX)
 bool has_mouse = false;
-#else
-bool has_mouse = true;
-#endif
 bool mouse_has_three_buttons = true;
 
 JE_word lastMouseX, lastMouseY;
@@ -90,24 +86,9 @@ void JE_grabShapeTypeOne( JE_word x, JE_word y, JE_byte *shape )
 
 void JE_mouseStart( void )
 {
-	const JE_word mouseCursorGr[3] /* [1..3] */ = {273, 275, 277};
-	
-	if (has_mouse)
-	{
-		service_SDL_events(false);
-		mouseButton = mousedown ? lastmouse_but : 0; /* incorrect, possibly unimportant */
-		lastMouseX = MIN(mouse_x, 320 - 13);
-		lastMouseY = MIN(mouse_y, 200 - 16);
-		
-		JE_grabShapeTypeOne(lastMouseX, lastMouseY, mouseGrabShape);
-		
-		blit_sprite2x2(VGAScreen, lastMouseX, lastMouseY, shapes6, mouseCursorGr[mouseCursor]);
-	 }
 }
 
 void JE_mouseReplace( void )
 {
-	if (has_mouse)
-		JE_drawShapeTypeOne(lastMouseX, lastMouseY, mouseGrabShape);
 }
 
